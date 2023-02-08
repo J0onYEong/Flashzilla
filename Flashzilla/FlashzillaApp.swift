00edc2c6553740350a72effffbb60e99dfbf983b
choijun0 <cjy00050005@gmail.com>
2023-02-08 14:15:12 +0900

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by 최준영 on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
